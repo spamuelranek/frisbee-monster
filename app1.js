d51b12892346a11a05cdb16e75be22fbae146e21
Samuel Panek <samuelrpanek@gmail.com>
2021-09-04 09:10:11 -0700

    removed while loop as unneeded

--- app1.js
+++ app1.js
@@ -4,37 +4,51 @@ function aliensG() {
             document.getElementById("promptA").innerHTML = " A Learned Person";
 
         } else if(userAnswer.toLowerCase() == "no"){
-            document.getElementById(promptA).innerHTML = " A soon to be Convert";
+            document.getElementById("promptA").innerHTML = " A soon to be Convert";
         }
 }
 
-// function centralAnswer() {
-//     let whereAnswer = document.getElementById("centralPrompt").value
-//     console.log(whereAnswer)
-//     while(whereAnswer.toLowerCase() == "central park"){
-
-//         if(whereAnswer.toLowerCase() !== "central park"){
-//             document.getElementById("centralAnswer").innerHTML = "Try Again";
-
-//         }
-//             document.getElementById("centralAnswer").innerHTML = "That's Correct"
-
-//     }
-// }
-
-function cAnswer() {
-    let whereAnswer = prompt( "Where did the item land")
+function centralAnswer() {
+    let whereAnswer = document.getElementById("centralPrompt").value
     console.log(whereAnswer)
-    while(whereAnswer.toLowerCase() !== "central park"){
-        if(whereAnswer.toLocaleLowerCase () == "central park"){
-            alert(" That's Correct");
-            console.log("success")
-            break;
-        } else if(whereAnswer.toLowerCase() !=="central park"){
-            alert(" Try Again");
-            whereAnswer = prompt( "Where did the item land") 
+        if(whereAnswer.toLowerCase() !== "central park"){
+            document.getElementById("centralAnswer").innerHTML = "Try Again";
+
         }
-    }
+        else if(whereAnswer.toLowerCase() == "central park"){
+            document.getElementById("centralAnswer").innerHTML = "That's Correct"
+        }
+
+}
+
+function bigAnswer() {
+    let sizeAnswer = document.getElementById("frisbeePrompt").value
+    console.log(sizeAnswer)
+        if(sizeAnswer !== "60" ){
+            document.getElementById("frisbeeAnswer").innerHTML = "Try Again";
+
+        }
+        else if(sizeAnswer == "60" ){
+            document.getElementById("frisbeeAnswer").innerHTML = "That's Correct"
+        }
+
+}
+
+
+// function cAnswer() {
+//     let correctAnswer = "a" ;
+//     let whereAnswer = prompt( "Where did the item land");
+//     console.log(whereAnswer)
+//     while(whereAnswer.toLowerCase() == correctAnswer){
+//         if(whereAnswer.toLocaleLowerCase () == correctAnswer){
+//             alert(" That's Correct");
+//             console.log("success");
+//             break;
+//         } else if(whereAnswer.toLowerCase() !== correctAnswer){
+//             alert(" Try Again");
+//             whereAnswer = prompt( "Where did the item land");
+//         }
+//     }
 
         
-}
+// }
