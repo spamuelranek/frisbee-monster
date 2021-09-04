3356263dc14404b91a25840e4c253325d1be3406
Samuel Panek <samuelrpanek@gmail.com>
2021-09-04 09:12:06 -0700

    actual final changes

--- app1.js
+++ app1.js
@@ -10,12 +10,13 @@ function aliensG() {
 
 function centralAnswer() {
     let whereAnswer = document.getElementById("centralPrompt").value
+    let correctWhereanswer = "central park"
     console.log(whereAnswer)
-        if(whereAnswer.toLowerCase() !== "central park"){
+        if(whereAnswer.toLowerCase() !== correctWhereanswer ){
             document.getElementById("centralAnswer").innerHTML = "Try Again";
 
         }
-        else if(whereAnswer.toLowerCase() == "central park"){
+        else if(whereAnswer.toLowerCase() == correctWhereanswer ){
             document.getElementById("centralAnswer").innerHTML = "That's Correct"
         }
 
@@ -23,32 +24,33 @@ function centralAnswer() {
 
 function bigAnswer() {
     let sizeAnswer = document.getElementById("frisbeePrompt").value
+    let correctSizeanswer = "60"
     console.log(sizeAnswer)
-        if(sizeAnswer !== "60" ){
+        if(sizeAnswer !== correctSizeanswer ){
             document.getElementById("frisbeeAnswer").innerHTML = "Try Again";
 
         }
-        else if(sizeAnswer == "60" ){
+        else if(sizeAnswer == correctSizeanswer ){
             document.getElementById("frisbeeAnswer").innerHTML = "That's Correct"
         }
 
 }
 
 
-// function cAnswer() {
-//     let correctAnswer = "a" ;
-//     let whereAnswer = prompt( "Where did the item land");
-//     console.log(whereAnswer)
-//     while(whereAnswer.toLowerCase() == correctAnswer){
-//         if(whereAnswer.toLocaleLowerCase () == correctAnswer){
-//             alert(" That's Correct");
-//             console.log("success");
-//             break;
-//         } else if(whereAnswer.toLowerCase() !== correctAnswer){
-//             alert(" Try Again");
-//             whereAnswer = prompt( "Where did the item land");
-//         }
-//     }
+function cAnswer() {
+    let correctAnswer = "a" ;
+    let whereAnswer = prompt( "Where did the item land");
+    console.log(whereAnswer)
+    while(whereAnswer.toLowerCase() == correctAnswer){
+        if(whereAnswer.toLocaleLowerCase () == correctAnswer){
+            alert(" That's Correct");
+            console.log("success");
+            break;
+        } else if(whereAnswer.toLowerCase() !== correctAnswer){
+            alert(" Try Again");
+            whereAnswer = prompt( "Where did the item land");
+        }
+    }
 
         
-// }
+}
